01f0868a5718728a4097bc77d2e0289f98f45b04
Tessa Gerasimova <tessagerasimova@gmail.com>
2025-12-31 18:51:48 +0200

    add api

---
+++ yatube_api/api/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from api.permissions import AuthorOrReadOnly
+from api.serializers import PostSerializer, GroupSerializer, CommentSerializer
+from posts.models import Post, Group
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    permission_classes = (AuthorOrReadOnly, IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly, IsAuthenticated,)
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs['post_id'])
+
+    def get_queryset(self):
+        return self.get_post().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(post=self.get_post(), author=self.request.user)
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,10 +1,21 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework import routers
+from rest_framework.authtoken import views
+from api.views import PostViewSet, GroupViewSet, CommentViewSet
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'groups', GroupViewSet, basename='group')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comment'
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/v1/api-token-auth/', views.obtain_auth_token),
+    path('api/v1/', include(router.urls)),
 ]
 
 
